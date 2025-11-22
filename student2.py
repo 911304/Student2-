583f862130bf3dc06c4867e9b8b87e3eca5c794e
911304 <veenarayanaikar@gmail.com>
2025-11-22 11:20:07 +0530

    Update student2.py

--- student2.py
+++ student2.py
@@ -1,16 +1,16 @@
-import sys
+ import sys
 
 if len(sys.argv) == 3:
     script_name = sys.argv[0]
     name = sys.argv[1]
-    rollno = sys.argv[2]
-    print("Usare provided input values:")
+    rollno = sys.argv[2] 
+    print("User provided input values:")
 else:
     script_name = sys.argv[0]
-    name = "veena"
+    name = "Veena"  
     rollno = "280"
     print("No input given - using default values:")
 
-print("Script Name:",script_name)
-print("Student Name:",name)
-print("Roll Number:",rollno)
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
